2825117f6e1f2e41a633c290fa94c29669620159
Ric Grant <39122715+qwebltd@users.noreply.github.com>
2024-10-10 10:58:11 +0100

    Update ffmpeg-to-youtube-feed-for-raspberry-pi.sh
    Rewritten to wrap everything in a self-recovery loop, monitoring ffmpeg for an exit and re-spawning if not a purposeful kill instead of having to wait for the cron to kick in.
    
    Also added no_duration_filesize flv flags to prevent glitches with attempting to write back to the rtmp file after the stream ends.
    
    Amended the instructive comments slightly because now, if running directly, the script should be invoked as a background process since it now waits for ffmpeg to exit before returning.

--- Bash scripts for Linux/ffmpeg-to-youtube-feed-for-raspberry-pi.sh
+++ Bash scripts for Linux/ffmpeg-to-youtube-feed-for-raspberry-pi.sh
@@ -2,13 +2,13 @@
 
 # Video streaming script by QWeb Ltd, using ffmpeg and a USB camera on a Raspberry Pi 4 B
 
-# Just populate the STREAM_URL and STREAM_KEY variables with your Youtube stream credentials and run this script to start streaming.
+# Just populate the STREAM_URL and STREAM_KEY variables with your Youtube stream credentials and run this script as a background process to start streaming.
 
 # wget https://raw.githubusercontent.com/qwebltd/Useful-scripts/main/Bash%20scripts%20for%20Linux/ffmpeg-to-youtube-feed-for-raspberry-pi.sh -O youtube-feed.sh
 # chmod +x ./youtube-feed.sh
 # nano ./youtube-feed.sh
 #   (edit credentials)
-# ./youtube-feed.sh
+# ./youtube-feed.sh &
 
 # For a totally headless set-up, add this script to your crontab to launch on boot and relaunch every 10 minutes (thus self-resolving any stream issues that might occur):
 
@@ -16,6 +16,8 @@
 #   (add the following lines)
 # @reboot /path/to/youtube-feed.sh
 # */10 * * * * /path/to/youtube-feed.sh
+
+# This script also respawns ffmpeg automatically if it exits for any reason other than a purposeful kill signal.
 
 # NOTE:
 # 5v 3a power in required, else fps suffers because USBs become under-supplied. Most phone chargers are only 1.8a - 2a
@@ -41,22 +43,45 @@ VERBOSITY=""
 # Comment this line if debugging. Otherwise ffmpeg won't output anything unless an error occurs
 VERBOSITY="${VERBOSITY} -hide_banner -loglevel error -y"
 
-# Wait for a connection
-while ! ping -c 1 -W 1 a.rtmp.youtube.com &> /dev/null; do
-  echo "Waiting for connection..."
-  sleep 0.1
+# We need to initialise this with something other than 255 to kick-start the main loop
+ffmpeg_exit_code=""
+
+# The main loop
+while [[ $ffmpeg_exit_code != "255" ]]; do
+  # Kill any previous iterations of this script, to prevent cron from creating overlapping instances
+  # It isn't ideal to do this before the connection check because we might hit latency there, but if we do the check first and get stuck there, we could end up spawning infinite processes
+  kill -9 $(pgrep -f ${BASH_SOURCE[0]} | grep -v $$) &> /dev/null
+
+  # Wait for a connection
+  while ! ping -c 1 -W 1 a.rtmp.youtube.com &> /dev/null; do
+    echo "Waiting for connection..."
+    sleep 0.1
+  done
+
+  echo "Connection ready."
+
+  # Now kill any old instances of ffmpeg. It should be safe to wait until after the connection check for this
+  killall ffmpeg &> /dev/null
+
+  # Wait until the v4l2 device is free again and then set it up
+  while ! v4l2-ctl $VIDEO_FOR_LINUX_CONTROLS &> /dev/null; do
+    echo "Waiting for device..."
+    sleep 0.1
+  done
+
+  echo "Device ready."
+
+  # Launch ffmpeg and grab its PID
+  ffmpeg -nostdin $VERBOSITY -use_wallclock_as_timestamps 1 -re -fflags +genpts+nobuffer $AUDIO_IN $VIDEO_IN -codec:a $AUDIO_OUT -codec:v $VIDEO_OUT -f flv -flvflags no_duration_filesize $STREAM_URL/$STREAM_KEY &
+  ffmpeg_process=$!
+
+  echo "Stream started."
+
+  # Wait for that PID to exit and grab its exit code
+  wait $ffmpeg_process
+  ffmpeg_exit_code=$?
+
+  echo "Stream ended with exit code $ffmpeg_exit_code."
+
+  # We'll loop back around if the exit code is not 255, i.e. ffmpeg was not putposefully killed with kill or killall commands
 done
-
-# Make sure ffmpeg isn't still running from previous iterations
-killall ffmpeg &> /dev/null
-
-# And kill any previous iterations of this script, since they might be stuck in the waiting for connection state
-kill -9 $(pgrep -f ${BASH_SOURCE[0]} | grep -v $$) &> /dev/null
-
-# Then run, but wait until the device is ready again because the above kills can take a moment
-while ! v4l2-ctl $VIDEO_FOR_LINUX_CONTROLS &> /dev/null; do
-  echo "Waiting for device..."
-  sleep 0.1
-done
-
-ffmpeg -nostdin $VERBOSITY -use_wallclock_as_timestamps 1 -re -fflags +genpts+nobuffer $AUDIO_IN $VIDEO_IN -codec:a $AUDIO_OUT -codec:v $VIDEO_OUT -f flv $STREAM_URL/$STREAM_KEY &
